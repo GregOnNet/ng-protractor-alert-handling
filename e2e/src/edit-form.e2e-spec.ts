26d95f7983307c8d1f6bbe29639735905789d43e
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-05-22 11:38:52 +0200

    test: add negative test

--- e2e/src/edit-form.e2e-spec.ts
+++ e2e/src/edit-form.e2e-spec.ts
@@ -1,16 +1,27 @@
 import { browser, $ } from 'protractor';
+import { AlertPromise } from 'selenium-webdriver';
 
 describe('<app-edit-form>', () => {
   describe('When the form page is left', () => {
-    it('navigates to home if user confirms to leave the page', async () => {
+    let confirmDialog: AlertPromise;
+
+    beforeEach(async () => {
       await browser.get(`${browser.baseUrl}edit`);
       await $('[name=link-home').click();
 
-      const confirmDialog = await browser.switchTo().alert();
+      confirmDialog = (await browser.switchTo().alert()) as AlertPromise;
+    });
 
+    it('navigates to home if user confirms to leave the page', async () => {
       await confirmDialog.accept();
 
       expect(browser.getCurrentUrl()).toBe('http://localhost:4200/');
+    });
+
+    it('stays on edit page if dialog is dismissed', async () => {
+      await confirmDialog.dismiss();
+
+      expect(browser.getCurrentUrl()).toBe('http://localhost:4200/edit');
     });
   });
 });
